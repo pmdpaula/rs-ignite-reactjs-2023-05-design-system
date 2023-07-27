45a013cffb9085751c4910fde58c880e5d458a60
Pedro Machado de Paula <pmdpaula@gmail.com>
2023-07-26 21:56:57 -0300

    feat(component): componente textinput

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,38 @@
+import { StoryObj, Meta } from '@storybook/react';
+import { TextInput, TextInputProps, Box, Text } from '@ignite-ui/react';
+
+
+export default {
+  title: 'Form/TextInput',
+  component: TextInput,
+  tags: ['autodocs'],
+  args: {
+    placeholder: 'placeholder',
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box as='label' css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}>
+          <Text size='sm'>E-mail address</Text>
+          {Story()}</Box>
+      );
+    }
+  ],
+} as Meta<TextInputProps>;
+
+type Story = StoryObj<TextInputProps>;
+
+export const Primary: Story = {};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  }
+};
+
+export const WithPrefix: Story = {
+  args: {
+    prefix: 'cal.com/',
+  }
+};
+
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,6 @@
 export * from './components/Box';
 export * from './components/Text';
+export * from './components/TextInput';
 export * from './components/Heading';
 export * from './components/Avatar';
 export * from './components/Button';
