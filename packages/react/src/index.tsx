cc33a25b84f0148ba754103a8f6ce9685343d11f
Pedro Machado de Paula <pmdpaula@gmail.com>
2023-08-10 17:45:26 -0300

    refactor(components): Add option to pass references to TextInput and adjust some styles

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -7,3 +7,5 @@ export * from './components/Avatar';
 export * from './components/Button';
 export * from './components/Checkbox';
 export * from './components/MultiStep';
+
+export * from './styles';
